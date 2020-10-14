963f67544b582fa4277a6d7a1ee6118c86e89371
Calvin Walantus <nivlac237@gmail.com>
2020-10-13 18:22:04 -0700

    CharacterController test scene

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+/**
+ * General purpose game controller attached to the GameController object in scene.
+ * */
+public class GameController : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }
+    }
+}
