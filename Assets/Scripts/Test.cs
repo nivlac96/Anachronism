066050bcd0c93bd3ddfde1a046fa3ad6363b5983
Calvin Walantus <nivlac237@gmail.com>
2020-11-20 18:34:44 -0800

    Test scene assets

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public bool isMoving = true;
+    public float speed = 2;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().enabled = false; ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMoving)
+        {
+            gameObject.transform.position = new Vector2(gameObject.transform.position.x + speed * Time.deltaTime, gameObject.transform.position.y);
+        }
+    }
+}
